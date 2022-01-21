b30f74f9daabedefcbff84c49bc181f207c97596
Yosvel Quintero <yosvelquintero@gmail.com>
2022-01-21 20:45:11 +0700

    Code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Server } from 'http';
+import { INestApplication } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as awsServerlessExpress from 'aws-serverless-express';
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  Context,
+} from 'aws-lambda';
+import { createServer, proxy } from 'aws-serverless-express';
+import { Server } from 'http';
 import * as express from 'express';
+
+import { AppModule } from './app.module';
 
 let cachedServer: Server;
 
-const bootstrapServer = async (): Promise<Server> => {
+const bootstrapServer: () => Promise<Server> = async (): Promise<Server> => {
   const expressApp = express();
-  const adapter = new ExpressAdapter(expressApp);
-  const app = await NestFactory.create(AppModule, adapter);
+  const adapter: ExpressAdapter = new ExpressAdapter(expressApp);
+  const app: INestApplication = await NestFactory.create(AppModule, adapter);
   app.enableCors();
   await app.init();
-  return awsServerlessExpress.createServer(expressApp);
-}
+  return createServer(expressApp);
+};
 
-export const handler: APIGatewayProxyHandler = async (event, context) => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<any> => {
   if (!cachedServer) {
-    cachedServer = await bootstrapServer()
+    cachedServer = await bootstrapServer();
   }
-  return awsServerlessExpress.proxy(cachedServer, event, context, 'PROMISE')
-      .promise;
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
 };
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('AppController (e2e)', (): void => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeEach(
+    async (): Promise<void> => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    },
+  );
 
-  it('/ (GET)', () => {
+  it('/ (GET)', (): Test => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
